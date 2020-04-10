e221c7b04d296fd9b72428cd3b96b57aa5fe5b1d
Tom Insam <tom@movieos.org>
2020-04-10 13:23:04 -0700

    integrate dropbox

--- PhotoSync/AppDelegate.swift
+++ PhotoSync/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import SwiftyDropbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        DropboxClientsManager.setupWithAppKey("ru820t3myp7s6vk")
         return true
     }
 
