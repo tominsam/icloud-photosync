61c8a0b63d650625c7bac9c01382d6000355e35c
Tom Insam <tom@movieos.org>
2020-04-10 13:06:13 -0700

    Initial Commit

---
+++ PhotoSync/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoSync
+//
+//  Created by Thomas Insam on 4/10/20.
+//  Copyright © 2020 Thomas Insam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
