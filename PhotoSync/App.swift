c8f8630522ecd7c767c30817327c842cf898d710
Tom Insam <tominsam@dropbox.com>
2026-05-06 19:26:59 -0400

    debug logging

--- PhotoSync/App.swift
+++ PhotoSync/App.swift
@@ -12,7 +12,21 @@ struct PhotoSyncApp: App {
 
     init() {
         DropboxClientsManager.setupWithAppKey("ru820t3myp7s6vk")
+        logTempDirectory()
         AssetData.cleanupTempDirectory()
+    }
+
+    private func logTempDirectory() {
+        let tmp = FileManager.default.temporaryDirectory
+        NSLog("%@", "Temp directory: \(tmp.path)")
+        guard let enumerator = FileManager.default.enumerator(at: tmp, includingPropertiesForKeys: [.fileSizeKey], options: .skipsHiddenFiles) else { return }
+        var totalBytes = 0
+        for case let url as URL in enumerator {
+            let size = (try? url.resourceValues(forKeys: [.fileSizeKey]).fileSize) ?? 0
+            totalBytes += size
+            NSLog("%@", "  \(url) (\(ByteCountFormatter.string(fromByteCount: Int64(size), countStyle: .file)))")
+        }
+        NSLog("%@", "Temp total: \(ByteCountFormatter.string(fromByteCount: Int64(totalBytes), countStyle: .file))")
     }
 
     var body: some Scene {
